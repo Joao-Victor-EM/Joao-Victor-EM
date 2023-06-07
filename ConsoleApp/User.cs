6cd69ae6d5783ae3f30f81e1d87517556e72e043
Joao-Victor-EM <joao.victor@escolarmanager.com.br>
2023-06-07 14:02:32 -0300

    add base explanation inplicit imports

---
+++ ConsoleApp/User.cs
@@ -0,0 +1,8 @@
+public class User
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public string? Gender { get; set; }
+    public string? Status { get; set; }
+}
